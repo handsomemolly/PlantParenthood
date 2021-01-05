db74aec2705c05ca24f5cb8673c19379f102bb1a
handsomemolly <ryan.kendig@gmail.com>
2021-01-05 09:44:58 -0800

    started sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    # before_action :require_login
+
+    def new
+    end
+
+
+    def create session[:name] = params[:name]
+        redirect_to '/'
+    end
+
+    def destroy
+        session.delete :name
+    end
+
+
+    private
+
+    def require_login
+        return head(:forbidden) unless session.include? :name
+    end
+end
 end
