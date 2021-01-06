b8f812d1321426bc8e5cdd1d121418d9d420f0cd
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-06 14:23:34 -0800

    notes almost finished

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,17 @@
 class NotesController < ApplicationController
+  def new
+    @note = Note.new
+    @user_plants = @user.user_plants
+  end
+
+  def create
+    @note = Note.create(note_params)
+    redirect_to @user
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:user_plant_id, :add_care)
+  end
 end
--- app/models/user_plant.rb
+++ app/models/user_plant.rb
@@ -1,4 +1,5 @@
 class UserPlant < ApplicationRecord
   belongs_to :user
   belongs_to :plant
+  has_many :notes
 end
